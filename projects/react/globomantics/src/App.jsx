adb4ad72b0dfe0f716607d6b482050a3c1355d3b
patricklebreton@gmail.com <patricklebreton@gmail.com>
2025-06-13 09:30:06 +0100

    react stuff

--- projects/react/globomantics/src/App.jsx
+++ projects/react/globomantics/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,7 +9,9 @@ function App() {
 
   return (
     <>
+      
       <div>
+        
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
---
+++ projects/react/globomantics/src/components/Banner.jsx
@@ -0,0 +1,13 @@
+import logo from "../assets/Globolgo.png"
+
+const Banner=()=>{
+    return (
+        <header>
+            <div>
+                <img src={logo} alt="logo"/>
+            </div>
+            <div>Providing House All Over the World</div>
+        </header>
+    );
+}
+export default Banner;
